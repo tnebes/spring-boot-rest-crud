28f09a09e2985aa21e8a002c0d00faf331e2b9f7
tnebes <tomislav.nebes@gmail.com>
2022-09-25 09:23:17 +0200

    SBRC-001 Added database and user initialiser.

--- src/main/java/tnebes/hr/crud/CrudApplication.java
+++ src/main/java/tnebes/hr/crud/CrudApplication.java
@@ -2,11 +2,13 @@ package tnebes.hr.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tnebes.hr.crud.util.StartupUtil;
 
 @SpringBootApplication
 public class CrudApplication {
 
 	public static void main(String[] args) {
+		StartupUtil.init();
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
---
+++ src/main/java/tnebes/hr/crud/util/StartupUtil.java
@@ -0,0 +1,12 @@
+package tnebes.hr.crud.util;
+
+public class StartupUtil {
+
+    public static void init() {
+        System.out.println("Validating database and user.");
+        
+
+
+    }
+
+}
