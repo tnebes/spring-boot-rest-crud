c33c82d07c150e5053a573e8bf36c52646f743fd
tnebes <tomislav.nebes@gmail.com>
2023-03-29 18:37:22 +0200

    SBRC-006 Updated tests

--- src/test/java/hr/tnebes/crud/controllers/impl/ProductCreateControllerImplTest.java
+++ src/test/java/hr/tnebes/crud/controllers/impl/ProductCreateControllerImplTest.java
@@ -8,28 +8,25 @@ import hr.tnebes.crud.models.ProductModel;
 import hr.tnebes.crud.repository.ProductRepository;
 import hr.tnebes.crud.services.ProductService;
 import hr.tnebes.crud.utils.Constants;
-import org.junit.jupiter.api.Assertions;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(ProductCreateControllerImpl.class)
 class ProductCreateControllerImplTest {
 
     @Autowired
@@ -44,58 +41,43 @@ class ProductCreateControllerImplTest {
     @MockBean
     private ProductMapper productMapper;
 
-//    @Test
-//    void createProduct_returnsCreated() throws Exception {
-//
-//        final ProductDto productDto = new ProductDto();
-//
-//
-//        final ProductModel productModel = new ProductModel();
-//
-//        Mockito.when(this.productMapper.toModel(productDto)).thenReturn(productModel);
-//        Mockito.when(this.productRepository.save(productModel)).thenReturn(productModel);
-//
-//        this.mockMvc.perform(post("/api/v1/product/create")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(new ObjectMapper().writeValueAsString(productDto)))
-//                .andExpect(status().isCreated());
-//
-//        Mockito.verify(this.productMapper, times(1)).toModel(productDto);
-//        Mockito.verify(this.productRepository, times(1)).save(productModel);
-//    }
+    @DisplayName("GIVEN a valid product dto WHEN sent to controller THEN return the product model.")
+    @Test
+    void testCreateProductReturnsProductModelOnValidDto() throws Exception {
+        final ProductDto productDto = TestConstants.VALID_PRODUCT_DTO;
+        final ProductModel productModel = TestConstants.VALID_PRODUCT_MODEL;
+        Mockito.when(this.productMapper.toModel(productDto)).thenReturn(productModel);
+        Mockito.when(this.productRepository.save(productModel)).thenReturn(productModel);
+        this.mockMvc.perform(post("/api/v1/product/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(productDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.code").value(productDto.getCode()))
+                .andExpect(jsonPath("$.name").value(productDto.getName()))
+                .andExpect(jsonPath("$.priceEur", Matchers.equalTo(productDto.getPriceEur().doubleValue())))
+                .andExpect(jsonPath("$.priceHrk", Matchers.equalTo(productDto.getPriceHrk().doubleValue())))
+                .andExpect(jsonPath("$.description").value(productDto.getDescription()))
+                .andExpect(jsonPath("$.availability").value(productDto.getAvailability().toString()));
+        Mockito.verify(this.productMapper, times(1)).toModel(productDto);
+        Mockito.verify(this.productRepository, times(1)).save(productModel);
+    }
 
-    @DisplayName("GIVEN WHEN THEN")
+    @DisplayName("GIVEN an invalid product dto WHEN sent to controller THEN return an error message.")
     @Test
     void testCreateProductReturnsExceptionOnInvalidProduct() throws Exception {
-        final ProductDto productDto = TestConstants.INVALID_PRODUCT_DTO;
-        final RestTemplate restTemplate = new RestTemplate();
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        final HttpEntity<String> requestEntity = new HttpEntity<>(new ObjectMapper().writeValueAsString(productDto), headers);
-        final URI uri = new URI("http://localhost:8080/api/v1/product/create");
-        Assertions.assertAll(
-                () -> Assertions.assertThrows(HttpClientErrorException.class, () -> restTemplate.postForEntity(uri, requestEntity, ProductModel.class))
-        );
+        final ProductDto productDto = TestConstants.EMPTY_PRODUCT_DTO;
+        this.mockMvc.perform(post("/api/v1/product/create")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(new ObjectMapper().writeValueAsString(productDto)))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$", hasItem(containsString("priceEur must not be null"))))
+                .andExpect(jsonPath("$", hasItem(containsString("priceHrk must not be null"))))
+                .andExpect(jsonPath("$", hasItem(containsString("availability must not be null"))))
+                .andExpect(jsonPath("$", hasItem(containsString("code must not be blank"))))
+                .andExpect(jsonPath("$", hasItem(containsString("name must not be blank"))))
+                .andExpect(jsonPath("$", hasItem(containsString("quantity must not be null"))));
         Mockito.verify(this.productMapper, never()).toModel(productDto);
         Mockito.verify(this.productRepository, times(Constants.FAKE_DATA_COUNT)).save(Mockito.any(ProductModel.class));
     }
-
-//    @Test
-//    void createProduct_returnsInternalServerError_whenSaveFails() throws Exception {
-//        final ProductDto productDto = new ProductDto();
-//
-//        final ProductModel productModel = new ProductModel();
-//
-//        Mockito.when(this.productMapper.toModel(productDto)).thenReturn(productModel);
-//        Mockito.when(this.productRepository.save(productModel)).thenThrow(new RuntimeException());
-//
-//        this.mockMvc.perform(post("/api/v1/product/create")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(new ObjectMapper().writeValueAsString(productDto)))
-//                .andExpect(status().isInternalServerError());
-//
-//        Mockito.verify(this.productMapper, times(1)).toModel(productDto);
-//        Mockito.verify(this.productRepository, times(1)).save(productModel);
-//    }
 
 }
